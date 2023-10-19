7342eba96b3c973847f90fab760b43c8a36d2c17
rugilesam <144427341+rugilesam@users.noreply.github.com>
2023-10-19 14:39:52 +0300

    Update studentai.h

--- studentai.h
+++ studentai.h
@@ -1,8 +1,18 @@
 #ifndef STUDENTAI_H
 #define STUDENTAI_H
 
-#include <string>
+include <string>
 #include <vector>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <numeric>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <limits>
 
 struct Duomenys {
     std::string vard, pav;
